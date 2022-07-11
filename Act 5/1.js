d609bb253df9c2e304476a1b523082ea4f6468a7
SaulPadilla6472 <saulpadilla.e@gmail.com>
2022-07-11 18:48:34 -0500

    Actividad 5

--- Act 5/1.js
+++ Act 5/1.js
@@ -1,5 +1,5 @@
 let array = [1, 4, 6, 10, 22, 55, 46, 2, 5, 0] 
-for(i=0; i<=array.length; i++){
+for(i=0; i<array.length; i++){
     var valor = array[i]
     if(valor>3){
         console.log(valor)
--- Act 5/3.js
+++ Act 5/3.js
@@ -1,5 +1,5 @@
 let array = [1, 4, 6, 10, 22, 55, 46, 2, 5, 0] 
-for(i=0; i<=array.length; i++){
+for(i=0; i<array.length; i++){
     array.sort(function(a,b){return a-b})
     console.log(array[i])
 }
