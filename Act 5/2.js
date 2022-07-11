b8cfec68feb8e0229c9bb7370830ab2d916b98cf
SaulPadilla6472 <saulpadilla.e@gmail.com>
2022-07-11 17:48:02 -0500

    Actividad 5

---
+++ Act 5/2.js
@@ -0,0 +1,7 @@
+let array = []
+var i = 0
+while(i<5){
+    array.push(i)
+    i++
+}
+console.log(array)
---
+++ Act 5/3.js
@@ -0,0 +1,5 @@
+let array = [1, 4, 6, 10, 22, 55, 46, 2, 5, 0] 
+for(i=0; i<=array.length; i++){
+    array.sort(function(a,b){return a-b})
+    console.log(array[i])
+}
